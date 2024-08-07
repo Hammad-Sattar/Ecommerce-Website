0360282293562f78837b79024513df8619166e39
Hammad <haadi@PROGRAMMER>
2024-08-07 18:40:09 +0500

    Add Identity to Project

--- Bulky.DataAcess/Data/ApplicationDbContext.cs
+++ Bulky.DataAcess/Data/ApplicationDbContext.cs
@@ -1,10 +1,12 @@
 ﻿using Bulky.Models.Models;
 using BulkyWeb.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace BulkyWeb.Data
 {
-    public class ApplicationDbContext:DbContext
+    public class ApplicationDbContext:IdentityDbContext<IdentityUser>
     {
         public ApplicationDbContext( DbContextOptions<ApplicationDbContext>options):base(options)
         {   
@@ -14,6 +16,7 @@ namespace BulkyWeb.Data
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            base.OnModelCreating(modelBuilder);
             modelBuilder.Entity<Category>().HasData(
                 new Category { Id = 1, Name = "Action", Displayorder = 1 },
                 new Category { Id = 2, Name = "Scifi", Displayorder = 2 },
--- BulkyWeb/Program.cs
+++ BulkyWeb/Program.cs
@@ -2,6 +2,7 @@ using Bulky.DataAcess.Repository;
 using Bulky.DataAcess.Repository.IRepository;
 using BulkyWeb.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 
 namespace BulkyWeb
 {
@@ -15,6 +16,8 @@ namespace BulkyWeb
             builder.Services.AddControllersWithViews();
             builder.Services.AddDbContext<ApplicationDbContext>(options=>
             options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+            builder.Services.AddRazorPages();
+            builder.Services.AddDefaultIdentity<IdentityUser>().AddEntityFrameworkStores<ApplicationDbContext>();
             builder.Services.AddScoped<IUnitOfWork,UnitOfWork>();
 
 
@@ -32,12 +35,15 @@ namespace BulkyWeb
             app.UseStaticFiles();
 
             app.UseRouting();
-
+            app.UseAuthentication();
             app.UseAuthorization();
+            app.MapRazorPages();
 
             app.MapControllerRoute(
                 name: "default",
                 pattern: "{controller=Home}/{action=Index}/{id?}");
+           
+
 
             app.Run();
         }
